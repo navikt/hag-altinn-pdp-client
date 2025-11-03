5a82366791170688d5c29ba1057871132bc75867
Gustav Berggren <gustav.berggren@nav.no>
2025-11-03 15:22:19 +0100

    Oppdater avhengigheter

--- src/test/kotlin/no/nav/helsearbeidsgiver/altinn/pdp/PdpResponseTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/altinn/pdp/PdpResponseTest.kt
@@ -2,7 +2,6 @@ package no.nav.helsearbeidsgiver.altinn.pdp
 
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
 import no.nav.helsearbeidsgiver.utils.test.resource.readResource
@@ -11,10 +10,10 @@ class PdpResponseTest :
     StringSpec({
         "Deserialiser PDP respons til objekt med riktig enum type" {
             listOf(
-                row("permit", Decision.Permit),
-                row("not-applicable", Decision.NotApplicable),
-                row("indeterminate", Decision.Indeterminate),
-                row("deny", Decision.Deny),
+                ("permit" to Decision.Permit),
+                ("not-applicable" to Decision.NotApplicable),
+                ("indeterminate" to Decision.Indeterminate),
+                ("deny" to Decision.Deny),
             ).forEach { (responseType, decision) ->
                 withClue("pdp response $responseType resulterer i Enum verdi ${Decision.Permit}") {
                     val validAltinnResponse = "pdp-response/$responseType.json".readResource()
